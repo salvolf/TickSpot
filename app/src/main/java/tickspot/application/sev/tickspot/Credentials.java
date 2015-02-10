9ef49082541738eef4ede949607361ab25948ca3
Salvo L. F <salvatorelafiura@mobilevikings.com>
2015-02-10 16:29:41 +0100

    .gitignore is now working

--- app/src/main/java/tickspot/application/sev/tickspot/Credentials.java
+++
@@ -1,8 +0,0 @@
-package tickspot.application.sev.tickspot;
-
-/**
- * Created by Sev on 10/02/15.
- */
-public class Credentials {
-   public final static String credentials = "salvatorelafiura@mobilevikings.com:PASSWORD";
-}
